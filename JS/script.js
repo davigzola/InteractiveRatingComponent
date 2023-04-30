76d3c7b0cefa0b3ccd6043dcaa23ea9821d1d17e
davigzola <davi.gzola@gmail.com>
2023-04-30 20:10:17 -0300

    Version 0.5

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,7 @@
-
-var grade1 = document.querySelector("#grade1");
-var grade2 = document.querySelector("#grade2");
-var grade3 = document.querySelector("#grade3");
-var grade4 = document.querySelector("#grade4");
-var grade5 = document.querySelector("#grade5");
-
-var grade = 5;
-
+var grades = document.querySelector(".grades");
 var frase = document.querySelector("#results-text");
 
-tempHTML = frase.innerHTML;
-newHTML = 'You selected ' + grade + ' out of 5';
-frase.innerHTML = newHTML;
-
-console.log();
-
+function choosenGrade() {
+    var selectedGrade = document.querySelector("input[type = radio]:checked").value;
+    return selectedGrade;
+}
